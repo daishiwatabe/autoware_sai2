0c520074eb5574f93e5f019cb3843a7e54e826bc
sai_hideyasu <sai.hideyasu2@gmail.com>
2019-05-10 17:28:12 +0900

    creating2

--- ros/src/util/packages/runtime_manager/scripts/runtime_manager_dialog.py
+++ ros/src/util/packages/runtime_manager/scripts/runtime_manager_dialog.py
@@ -70,6 +70,7 @@ from autoware_config_msgs.msg import ConfigPedestrianFusion
 from autoware_config_msgs.msg import ConfigPlannerSelector
 from autoware_config_msgs.msg import ConfigDecisionMaker
 from autoware_config_msgs.msg import ConfigCompareMapFilter
+from autoware_config_msgs.msg import ConfigLocalizerSwitchFusion
 from tablet_socket_msgs.msg import mode_cmd
 from tablet_socket_msgs.msg import gear_cmd
 from tablet_socket_msgs.msg import Waypoint
